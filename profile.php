3f57c7a42cf8621f41d1366c09a525c702b5b877
Jimbaz <james.barry26@mail.dcu.ie>
2016-11-17 20:31:42 +0000

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,17 @@
+<html>
+<head><link rel="stylesheet" type="text/css" href="main.css"></head>
+<body>
+	<div id="AppTitle">
+	<center><h1><font color = "black">Welcome to the Recipe App!</font></h1></center>
+	</div>
+	<div id="Recipeinput">
+	<form name="Recipe" method="post" action="session.php"> 
+		<center><font color = "black">Recipe Title:</font><center>
+			<input type="text" name="Title" value =""></center><p></p>
+			<center><font color = "black">Ingredients:</font></center><p></p>
+			<textarea cols="80" rows="10" name="Recipe"  type="text"></textarea>
+			<center><input type ="submit" name = "submit" value="Submit"></center>
+		</form> 
+		</div>
+	</body>
+	</html>
